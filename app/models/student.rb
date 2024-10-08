e190eb18ccc0899667b02abe395c6403cb57d576
Noah77707 <Noah.hoepfinger@gmail.com>
2024-10-08 19:13:50 +0000

    school email format works

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,12 @@
 class Student < ApplicationRecord
 
   validates :first_name, :last_name, :school_email, :major, :expected_graduation_date, presence: true
+  validate :school_email_format
+
+  def school_email_format
+    unless school_email =~ /\A[\w+\-.]+@msudenver\.edu\z/i
+      errors.add(:school_email, "must be an @msudenver.edu email address")
+    end
+  end
+
 end
