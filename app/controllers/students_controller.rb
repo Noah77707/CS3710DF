504951c7e71d2c2a91120677149ec0a912a10cf6
Noah77707 <Noah.hoepfinger@gmail.com>
2024-10-17 19:20:26 +0000

    Got the search students working

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,9 +2,23 @@ class StudentsController < ApplicationController
   before_action :set_student, only: %i[ show edit update destroy ]
 
   # GET /students or /students.json
+  # GET /students or /students.json
+  # GET /students or /students.json
   def index
+    Rails.logger.info "Params: #{params.inspect}"
+
+    @search_params = params[:search] || {}
     @students = Student.all
+
+    Rails.logger.info "Search Params: #{@search_params.inspect}"
+
+    if @search_params[:major].present?
+      @students = @students.where(major: @search_params[:major])
+    end
+
   end
+
+
 
   # GET /students/1 or /students/1.json
   def show
@@ -67,16 +81,5 @@ class StudentsController < ApplicationController
   def student_params
     params.require(:student).permit(:first_name, :last_name, :school_email, :major, :expected_graduation_date, :profile_picture)
   end
-
-  # # GET /students or /students.json
-  # def index
-  #   @search_params = params[:search] || {}
-  #   @students = Student.all
-  #
-  #   if @search_params[:major].present?
-  #     @students = @students.where(major: @search_params[:major])
-  #   end
-  #
-  # end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -29,4 +29,10 @@ class Student < ApplicationRecord
 
   end
 
+  VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS",
+                  "Computer Science BS", "Cybersecurity Major", "Data Science and Machine Learning Major"]
+
+  validates :major, inclusion: { in: VALID_MAJORS, message: "%{value} is not a valid major" }
+
+
 end
